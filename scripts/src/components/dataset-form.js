0a419038112d1dbdf902897ecc3e935125268893
RVCA GIS <gis@rvca.ca>
2018-02-26 11:44:30 -0500

    Update dataset-form.js

--- scripts/src/components/dataset-form.js
+++ scripts/src/components/dataset-form.js
@@ -1,13 +1,13 @@
 import $ from 'jquery'
-import 'select3'
+import 'select2'
 
 export default class {
   constructor (opts) {
     const elements = {
-      select3: $('.select3', opts.el)
+      select2: $('.select2', opts.el)
     }
 
     // Initialize select2 plugin
-    elements.select3.select3()
+    elements.select2.select2()
   }
 }
