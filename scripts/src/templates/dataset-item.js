64d19825ef29275e86bac7074f007cac7a5f09e4
RVCA GIS <gis@rvca.ca>
2019-10-11 01:27:47 -0400

    Update dataset-item.js

--- scripts/src/templates/dataset-item.js
+++ scripts/src/templates/dataset-item.js
@@ -1,7 +1,7 @@
 export default (data) => (
 `<dataset>
-  <h4><a href="${data.url}">${data.title}</a></h4>
-  <h5>${data.lastUpdate}</h5>
+  <a href="${data.url}">${data.title}</a>
+  ${data.lastUpdate}
   ${data.notes || ''}
 </dataset>`
 )
